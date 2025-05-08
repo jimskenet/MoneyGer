0d7655481d2d56db75678abeaf95802055aff766
James Kenneth <takashimaretubado@gmail.com>
2025-05-08 19:56:32 +0800

    frontend deploy

--- moneyger.client/src/app/shared/calendar.service.ts
+++ moneyger.client/src/app/shared/calendar.service.ts
@@ -1,14 +1,8 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
-import { LoginRequest } from '../interfaces/login-request';
-import { Observable, map } from 'rxjs';
+import { Observable} from 'rxjs';
 import { AuthResponse } from '../interfaces/auth-response';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { TmplAstSwitchBlockCase } from '@angular/compiler';
-import { jwtDecode } from 'jwt-decode';
-import { Role } from '../interfaces/role';
-import { CompanyRequest } from '../interfaces/company-request';
-import { CompanyJoin } from '../interfaces/company-join';
 import { AuthService } from './auth.service';
 import { EventRequest } from '../interfaces/event-request';
 import { EventAttendee } from '../interfaces/event-attendee';
